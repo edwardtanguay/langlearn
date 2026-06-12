109c7ce50bd57630985f069007617efa4343db96
Edward Tanguay (windows) <edward@tanguay.info>
2026-06-13 00:34:23 +0200

    DEV: Fixed various typescript errors etc.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,9 @@ export default defineNuxtConfig({
 	devtools: { enabled: true },
 
 	modules: ['@nuxt/ui', '@nuxtjs/color-mode', '@nuxtjs/kinde'],
+	icon: {
+		serverBundle: 'remote'
+	},
 	colorMode: {
 		preference: 'dark',
 		classSuffix: ''
